bcacc16acdea8b1a75453b5eabee0a5fef197a55
CristiMM00 <cristianus.micu@gmail.com>
2020-05-28 19:06:46 +0300

    Final

--- file.c
+++ file.c
@@ -3,7 +3,19 @@
 int main(int argc, char* argv[])
 {
 	int i;
-	for(i=1;i<argc;i++)
-		printf("%s \n",argv[i]);
+	char user[100];
+	int pid[10000];
+	FILE* users;
+	FILE* pids;
+	users=popen(" ps -Ao user", "r");
+	pids=popen(" ps -Ao pid", "r");
+	while(fgets(user,100,users) != NULL && fgets(pid,10000,pids) != NULL)
+	{
+		for(i=0;i<argc;i++)
+			if(user == argv[i])
+				system("kill pid ");
+	}	
+	pclose(users);
+	pclose(pids);
 	return 0;
 }
